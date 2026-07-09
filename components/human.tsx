45744398fbac1fe236bb70cd5ec42f27c661bafb
DaisyXUUUUU <daisy.ziyue0526@outlook.com>
2026-07-09 15:45:53 +0800

    fix

--- components/human.tsx
+++ components/human.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useState } from 'react'
 import Image from 'next/image'
-import { ChevronLeft, ChevronRight, Martini, Snowflake, Camera, Utensils, Trophy } from 'lucide-react'
+import { ChevronLeft, ChevronRight, Martini, Snowflake, Camera, Utensils, Trophy, Disc3 } from 'lucide-react'
 import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
 
 type Hobby = {
